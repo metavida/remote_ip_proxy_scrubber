2c4f33ed5c5f2379dc648973128320f1eb59b0c9
Marcos Wright-Kuhns <marcoswk@haikulearning.com>
2015-03-20 20:24:59 -0700

    Try to docompose IPList#to_regexp a bit

--- lib/remote_ip_proxy_scrubber/ip_list.rb
+++ lib/remote_ip_proxy_scrubber/ip_list.rb
@@ -9,6 +9,7 @@ module RemoteIpProxyScrubber
       add_ips(*given_ips)
     end
 
+    # Add one ore more IP addresses to the proxy_ips array
     def add_ips(*given_ips)
       @proxy_ips ||= []
       given_ips.each do |ip|
@@ -22,35 +23,53 @@ module RemoteIpProxyScrubber
       proxy_ips
     end
 
+    # Returns an Array of IPAddr values
     def proxy_ips
       @proxy_ips ||= []
     end
 
+    # Returns a Regexp that matches any IP address in proxy_ips
+    def to_regexp
+      Regexp.union(*proxy_ips.map { |ip| ip_addr_to_regexp(ip) })
+    end
+
+    # Iterate over the proxy_ips
     def each
       proxy_ips.each { |ip| yield(ip) }
     end
 
-    def to_regexp
-      Regexp.union(*proxy_ips.map do |ip|
-        range = ip.to_range
+    private
+
+    def ip_addr_to_regexp(ip)
+      range = ip.to_range
+      regexp_sources_for_range = (
         if range.first == range.last
-          Regexp.new("\\A#{ip}\\z")
+          [ip]
         else
-          low_ip_array  = range.first.to_s.split('.')
-          high_ip_array = range.last.to_s.split('.')
+          low_octets  = split_octets(range.first)
+          high_octets = split_octets(range.last)
 
           regexp_array = []
-          low_ip_array.each_with_index do |low_octet, octet_index|
-            high_octet = high_ip_array[octet_index]
-            if low_octet == high_octet
-              regexp_array << low_octet
-            else
-              regexp_array << RangeRegexp.new(low_octet..high_octet).regexp.source
-            end
+          4.times do |octet_index|
+            regexp_array << octet_pair_to_regexp_source(low_octets[octet_index], high_octets[octet_index])
           end
-          Regexp.new('\A' + regexp_array.join('\.') + '\z')
+          regexp_array
         end
-      end)
+      )
+
+      Regexp.new('\A' + regexp_sources_for_range.join('\.') + '\z')
+    end
+
+    def split_octets(ip)
+      ip.to_s.split('.')
+    end
+
+    def octet_pair_to_regexp_source(low_octet, high_octet)
+      if low_octet == high_octet
+        low_octet
+      else
+        RangeRegexp.new(low_octet..high_octet).regexp.source
+      end
     end
   end
 end
